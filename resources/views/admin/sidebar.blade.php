21de263fb0b5f30ef29b5c64d0d7f53b0f130879
Yogendra Neeladri <115712703+YogendraNeeladri@users.noreply.github.com>
2025-04-28 21:13:02 +0530

    Update sidebar.blade.php

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -5,7 +5,7 @@
         <div class="sidebar-header d-flex align-items-center">
           <div class="avatar"><img src="admin/img/avatar-6.jpg" alt="..." class="img-fluid rounded-circle"></div>
           <div class="title">
-            <h1 class="h5">Krishna Somu</h1>
+            <h1 class="h5">Giridhar Ruppa</h1>
             <p>The Fast Food Factory</p>
           </div>
         </div>
